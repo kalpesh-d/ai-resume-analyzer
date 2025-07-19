2ffbdc6c7f628178dbfe7d1d3f779d90d6a1c0a8
Kalpesh Dhoke <kalpeshdhoke7@gmail.com>
2025-07-19 11:04:04 +0530

    build auth functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import ResumeCard from "~/components/ResumeCard";
 import { resumes } from "../../constants/index";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +14,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')]">
       <Navbar />
+
       <section className="main-section py-16">
         <div className="page-heading">
           <h1>Track your Applications & Resume Ratings.</h1>
@@ -21,7 +34,7 @@ export default function Home() {
         </div>
 
         {resumes.length > 0 && (
-          <div className="resumes-secrtions">
+          <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
